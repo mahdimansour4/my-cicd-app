0e5e406469befeb84e4dbbd39c4af1279db175c1
Mahdi Mansour <mrmehdimido@gmail.com>
2025-04-23 18:01:49 +0100

    First Commit

---
+++ src/main/java/com/example/mycicdapp/MyCicdAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCicdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCicdAppApplication.class, args);
+	}
+
+}
